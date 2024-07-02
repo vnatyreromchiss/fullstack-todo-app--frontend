cf9ed1ca2b9fe017316c7b6748dc683adc1b7628
Romchiss <95075072+vnatyreromchiss@users.noreply.github.com>
2024-07-02 07:32:17 -0500

    Add files via upload

---
+++ src/utils/HandleAPI.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const baseUrl = "https://fullstack-todo-app-backend-1-br2c.onrender.com"; //http://localhost:5001 for local access
+
+const getAllToDo = (setToDo) => {
+  axios.get(baseUrl).then(({ data }) => {
+    console.log("data--->", data);
+    setToDo(data);
+  });
+};
+
+const addToDo = (text, setText, setToDo) => {
+  axios
+    .post(`${baseUrl}/save`, { text })
+    .then((data) => {
+      setText("");
+      getAllToDo(setToDo);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const updateToDo = (toDoId, text, setText, setToDo, setIsUpdating) => {
+  axios
+    .post(`${baseUrl}/update`, { _id: toDoId, text })
+    .then((data) => {
+      setText("");
+      setIsUpdating(false);
+      getAllToDo(setToDo);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+const deleteToDo = (_id, setToDo) => {
+  axios
+    .post(`${baseUrl}/delete`, { _id })
+    .then((data) => {
+      getAllToDo(setToDo);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export { getAllToDo, addToDo, updateToDo, deleteToDo };
